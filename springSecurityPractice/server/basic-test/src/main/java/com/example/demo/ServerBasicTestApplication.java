38de60218bf70555e8dffa0a4ecdfd94ae2cc00d
sangwon0724 <65121442+sangwon0724@users.noreply.github.com>
2022-09-28 01:14:53 +0900

    기본 파일 생성

--- springSecurityPractice/server/basic-test/src/main/java/com/example/demo/ServerBasicTestApplication.java
+++ springSecurityPractice/server/basic-test/src/main/java/com/example/demo/ServerBasicTestApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerBasicTestApplication {
 	//psvm을 자동 완성하면 main 함수가 된다.
 	public static void main(String[] args) {
-		Person person = new Person(); //롬복이 일을 안함
+		//Person person = new Person(); //롬복이 일을 안함
 		//Person person2 = Person.builder().name("test").build();
 		//System.out.println(person);
 		SpringApplication.run(ServerBasicTestApplication.class, args);
