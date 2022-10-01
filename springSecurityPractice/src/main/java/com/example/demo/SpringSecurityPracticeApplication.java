b2ed12d5b58042f9273aca2fbb7e500dffa90e5d
sangwon0724 <65121442+sangwon0724@users.noreply.github.com>
2022-10-01 19:13:03 +0900

    멀티 모듈일때 체크아웃 받는 방법

--- springSecurityPractice/src/main/java/com/example/demo/SpringSecurityPracticeApplication.java
+++ springSecurityPractice/src/main/java/com/example/demo/SpringSecurityPracticeApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 
-1. @SpringBootApplication을 읽어올수 없을 때는
-   프로젝트 우클릭 > Properties > Project Natures에서 Gradle Project Nature가 추가되어있나 확인하기
-2. 깃허브에서 받아왔을 때 [File 메뉴 > Import > Gradle > Existing Gradle Project를 실행 후 연결할 프로젝트 선택]을 진행하면
-   정상적으로 그래들이 연동된 상태의 프로젝트를 연결할수 있다.
+	1. @SpringBootApplication을 읽어올수 없을 때는
+	   프로젝트 우클릭 > Properties > Project Natures에서 Gradle Project Nature가 추가되어있나 확인하기
+	2. 깃허브에서 받아왔을 때 [File 메뉴 > Import > Gradle > Existing Gradle Project를 실행 후 연결할 프로젝트 선택]을 진행하면
+	   정상적으로 그래들이 연동된 상태의 프로젝트를 연결할수 있다.
+	3. 멀티 모듈의 경우 [Git Repositories > Working Tree에서 해당 모듈 선택 > 우클릭 > Import Projects...]를 진행하면
+	   디렉터리 구조가 아닌 프로젝트 구조로 check out 받게 된다. (Git의 경우)
 */
 
 @SpringBootApplication
