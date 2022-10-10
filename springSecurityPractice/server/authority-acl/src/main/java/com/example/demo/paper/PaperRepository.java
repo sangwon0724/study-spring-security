9b17f18f213c2298ac0fd3c80a3d5a870a74e12a
sangwon0724 <65121442+sangwon0724@users.noreply.github.com>
2022-10-10 14:32:33 +0900

    .

--- springSecurityPractice/server/authority-acl/src/main/java/com/example/demo/paper/PaperRepository.java
+++ springSecurityPractice/server/authority-acl/src/main/java/com/example/demo/paper/PaperRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.paper;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.
 
 import java.util.Optional;
 
